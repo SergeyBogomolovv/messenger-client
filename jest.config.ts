8738efa91717d4b135e7126bb30065d3bed3216c
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-05-02 12:13:52 +0300

    updated test environment

--- jest.config.ts
+++ jest.config.ts
@@ -1,20 +1,23 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
 import type { Config } from 'jest'
 
 const config: Config = {
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
-  coverageDirectory: 'coverage',
+  clearMocks: true,
+  coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/setup-tests.ts'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleNameMapper: {
     '^.+\\.svg$': 'jest-svg-transformer',
     '^.+\\.css$': 'identity-obj-proxy',
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: false,
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
 }
 
 export default config
